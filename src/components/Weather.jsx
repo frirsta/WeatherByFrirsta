55f61276fd742565945acd1bfe3f061ff134f7ec
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-09-01 20:14:05 +0200

    Refactor Weather component to improve city selection and data fetching logic
    - Integrated uuidv4 to generate unique keys for city suggestions in the Autocomplete component.
    - Simplified state management by handling city input as an object with  property.
    - Improved key handling and city option rendering to resolve warnings related to duplicate keys.
    - Enhanced error handling and input validation in the city search process.
    - Adjusted UI elements for better user experience in Autocomplete and search functionality.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
-import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const Weather = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState({ name: "" });
   const [options, setOptions] = useState([]);
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
@@ -29,6 +30,7 @@ const Weather = () => {
       determineDayOrNight(response.data);
       setError("");
       console.log(response.data, forecastResponse.data);
+      console.log(weather, forecast);
     } catch (e) {
       setError("Failed to fetch weather data. Please try again.");
       console.log(e);
@@ -67,35 +69,29 @@ const Weather = () => {
       );
       setOptions(
         response.data.list.map((city) => ({
-          key: `${city.name}-${city.sys.country}-${city.id}`,
           label: `${city.name}, ${city.sys.country}`,
           name: city.name,
+          id: uuidv4(),
         }))
       );
     } catch (e) {
       console.log("Failed to fetch city suggestions:", e);
     }
   };
-
-  const handleInputChange = (event, value) => {
-    if (typeof value === "string" && value.length > 2) {
-      fetchCitySuggestions(value);
-    } else if (typeof value === "object" && value !== null && value.name) {
-      setCity(value.name);
-    } else {
-      setOptions([]);
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+  const handleInputChange = (e) => {
+    if (e.target.value.length > 3) {
+      fetchCitySuggestions(e.target.value);
     }
   };
 
-  const handleChange = (event, newValue) => {
-    setCity(newValue ? newValue.name : "");
-  };
   const handleIconClick = () => {
-    if (city.trim() === "") {
+    if (city.name?.trim() === "") {
       setError("Please enter a city name");
       return;
     }
-
     fetchWeatherData();
   };
   const handleSubmit = (e) => {
@@ -113,19 +109,46 @@ const Weather = () => {
       <form onSubmit={handleSubmit}>
         <Autocomplete
           freeSolo
-          value={city ? { label: city } : null}
-          onInputChange={(event, value) => handleInputChange(event, value)}
-          options={options}
-          getOptionLabel={(option) => option.label}
           onChange={handleChange}
+          onInputChange={handleInputChange}
+          getOptionLabel={(option) => {
+            if (typeof option === "object" && option.label) {
+              return option.label;
+            }
+
+            if (typeof option === "string") {
+              return option;
+            }
+            return "";
+          }}
+          options={options}
           renderOption={(props, option) => (
-            <li {...props} key={option.key}>
+            <li {...props} key={option.id}>
               {option.label}
             </li>
           )}
           renderInput={(params) => (
             <TextField
               {...params}
+              size="small"
+              variant="outlined"
+              type="text"
+              placeholder="Enter city name"
+              slotProps={{
+                inputProps: { "aria-label": "search", value: city.name },
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton></IconButton> <SearchOutlinedIcon />
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <SearchOutlinedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
               sx={{
                 width: "100%",
                 "& .MuiInputBase-root": {
@@ -141,20 +164,6 @@ const Weather = () => {
                   },
                 },
               }}
-              slots={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={handleIconClick}>
-                      <SearchOutlinedIcon />
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              size="small"
-              variant="outlined"
-              type="text"
-              placeholder="Enter city name"
-              value={city}
             />
           )}
         />
@@ -173,36 +182,30 @@ const Weather = () => {
           <p>Wind speed: {weather.wind.speed} m/s</p>
         </Box>
       )}
-      {forecast && (
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container spacing={2}>
-            {forecast.map((item) => (
-              <Grid
-                size={3}
-                sx={{
-                  padding: "10px",
-                  border: "1px solid #ffffff",
-                  borderRadius: "10px",
-                  backdropFilter: "blur(5px)",
-                  boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
-                  background: "rgba(255, 255, 255, 0.50)",
-                }}
-                sm={{ padding: "100px" }}
-                key={item.dt}
-              >
-                <img
-                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                  alt={item.weather[0].description}
-                />
-                <h3>{item.weather[0].description}</h3>
-                <h3>{item.main.temp} °C</h3>
-                <p>Humidity: {item.main.humidity}</p>
-                <p>Wind speed: {item.wind.speed} m/s</p>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      )}
+      {forecast?.map((item) => (
+        <Grid
+          size={3}
+          sx={{
+            padding: "10px",
+            border: "1px solid #ffffff",
+            borderRadius: "10px",
+            backdropFilter: "blur(5px)",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            background: "rgba(255, 255, 255, 0.50)",
+          }}
+          sm={{ padding: "100px" }}
+          key={`${item.dt}`}
+        >
+          <img
+            src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+            alt={item.weather[0].description}
+          />
+          <h3>{item.weather[0].description}</h3>
+          <h3>{item.main.temp} °C</h3>
+          <p>Humidity: {item.main.humidity}</p>
+          <p>Wind speed: {item.wind.speed} m/s</p>
+        </Grid>
+      ))}
     </Box>
   );
 };
